f5883e0eaee25f33ec0438a02424f182beba6e79
Nazmul-hasan69 <hmdnazmul69@gmail.com>
2021-07-19 16:53:51 +0600

    frist modifie

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,11 +6,11 @@ $(function(){
     fade: true,
     speed:700,
     CssEase: 'linear',
-    //autoplay: true,
-    //autoplaySpeed: 2000,
+    autoplay: true,
+    autoplaySpeed: 2000,
     arrow: true,
-    prevArrow: '<i class="slider_arrow slider_arrow_left fas fa-chevron-left"></i>',
-    nextArrow: '<i class="slider_arrow slider_arrow_right fas fa-chevron-right"></i>',
+    prevArrow: '<i class="fas fa-chevron-left banner_arrow left"></i>',
+    nextArrow: '<i class="fas fa-chevron-right banner_arrow right"></i>',
   });
   $('.featured_filter_container').filterizr();
 })
